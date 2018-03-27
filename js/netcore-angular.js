139d9506042f593f21958e5732423ac7766fd0fe
Neville <live.neville@live.com>
2018-03-27 08:06:37 -0400

    initial code

---
+++ js/netcore-angular.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    angular.module('netcore-angular', [])
+
+        .directive('setToScope', function () {
+            return {
+                scope: true,
+                restrict: 'A',
+                controller: function ($attrs, $scope) {
+                    if (typeof (netcore_angular_pairs) !== "undefined"
+                        && typeof ($attrs.netcoreAngularSet) !== "undefined") {
+                        console.log(netcore_angular_pairs[$attrs.netcoreAngularSet]);
+                        $scope[$attrs.setToScope] = netcore_angular_pairs[$attrs.netcoreAngularSet];
+                    }
+                }
+            };
+        });
+
+})();
